50fc4791f0d435fd784c15ca6130671cb25c7db5
vipin <vipindubey1300@gmail.com>
2021-06-14 16:43:52 +0530

    importissue

--- lib/blocs/signup/bloc.dart
+++ lib/blocs/signup/bloc.dart
@@ -9,7 +9,6 @@ import 'package:slider_widget/models/models.dart';
 import 'package:slider_widget/utils/constants.dart';
 import 'package:slider_widget/utils/storage_helper.dart';
 import 'package:slider_widget/utils/validation_mixin.dart';
-import '../blocs.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:twitter_login/twitter_login.dart';
 import 'package:flutter_facebook_login/flutter_facebook_login.dart';
